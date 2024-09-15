3598d6d190195f0fb5ef172998ceb1d595d30a7f
Kalana Rubasinghe <kalanarajika99@gmail.com>
2024-09-15 21:52:20 +0530

    arg

--- server/crud/utils/HttpResponseUtil.py
+++ server/crud/utils/HttpResponseUtil.py
@@ -8,11 +8,11 @@ def to_json_response(data=None, status_code=200):
     return Response({"error": None, "data": data}, status_code)
 
 
-def to_json_error_response(status_code=400, code="", error_detail=None):
+def to_json_error_response(status_code=400, code="", error_detail=None, data=None):
     return Response({
         "error": {
             "code": code,
             "detail": error_detail
         },
-        "data": None,
+        "data": data,
     }, status=status_code)
