6bc3a76fcc92e4f99fde51299b9273d71e06a275
Kalana Rubasinghe <kalanarajika99@gmail.com>
2024-09-16 21:13:01 +0530

    model update

--- server/crud/models.py
+++ server/crud/models.py
@@ -2,11 +2,6 @@ import uuid
 from django.db import models
 from django.utils import timezone
 
-LOCATION_CHOICES = [
-        ('DESTINATION', 'Destination'),
-        ('CUSTOMER NUMBER', 'Customer Number'),
-        ('OUTLET ID', 'Outlet ID'),
-    ]
 
 class Event(models.Model):
     
@@ -16,7 +11,7 @@ class Event(models.Model):
     rc_num = models.CharField(max_length=100)  # A field for the rcNum value (can be adjusted for length)
     client_id = models.CharField(max_length=100)  # Client ID reference
     event_cnt = models.IntegerField(default=1)  # Integer for event count
-    location_cd = models.CharField(max_length=100, choices=LOCATION_CHOICES)  # Code for location
+    location_cd = models.CharField(max_length=100)  # Code for location
     location_id1 = models.CharField(max_length=50, blank=True, null=True)  # First location ID
     location_id2 = models.CharField(max_length=50, blank=True, null=True)  # Second location ID
     addr_nbr = models.CharField(max_length=50, blank=True, null=True)  # Address number
